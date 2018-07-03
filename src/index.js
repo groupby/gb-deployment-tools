b3b30f0668db5249107a37d2c4d15d68b4f46e7d
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-07-03 18:41:36 -0400

    Expose 'DATA' via module entry point.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const DATA = require( './data' );
 const GbDeploy = require('./gb-deploy');
 const GbRelease = require('./gb-release');
 const utils = require('./utils');
 
 module.exports = {
+	DATA,
 	GbDeploy,
 	GbRelease,
 	utils,
